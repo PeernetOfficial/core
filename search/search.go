3f14b10b3471c9800b2263c7203bb0e7681cbcb6
Akilan Selvacoumar <me@akilan.io>
2021-12-02 19:20:56 +0400

    added search

--- search/search.go
+++ search/search.go
@@ -4,9 +4,12 @@ import (
 	"errors"
 	"github.com/PeernetOfficial/core"
 	"github.com/PeernetOfficial/core/protocol"
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
 	"strings"
+	"unicode"
 )
-
 
 // GenerateIndexes This function generates various hashes based
 // on the filename provided.
@@ -17,19 +20,31 @@ func GenerateIndexes(text string) ([][]byte, error) {
 	if len(text) <= 3 {
 		return nil, errors.New("text needs to be more than 3 characters")
 	}
+
+	// Normalizes string provided in the parameter
+	normalizedStr, err := NormalizeWords(text)
+	if err != nil {
+		return nil, err
+	}
+
+	// normalized hash
+	normalizedHash := protocol.HashData([]byte(normalizedStr))
+
 	// Appending hash for the text string
 	hashes = append(hashes, protocol.HashData([]byte(text)))
+	// Appending normalized hash
+	hashes = append(hashes, normalizedHash)
 	// Appending lower case hash
-	hashes = append(hashes, LowerCaseHash(text))
+	hashes = append(hashes, LowerCaseHash(normalizedStr))
 	// Appending upper case hash
-	hashes = append(hashes, UpperCaseHash(text))
+	hashes = append(hashes, UpperCaseHash(normalizedStr))
 	// Appending split string by word
-	WordsHashes := HashByWordsSplit(text)
+	WordsHashes := HashByWordsSplit(normalizedStr)
 	for i := range WordsHashes {
 		hashes = append(hashes, WordsHashes[i])
 	}
 
-	err := core.InsertIndexRows(hashes, text)
+	err = core.InsertIndexRows(hashes, text)
 	if err != nil {
 		return nil, err
 	}
@@ -61,14 +76,44 @@ func RemoveIndexes(text string) error {
 
 // LowerCaseHash coverts string to lower case and returns the hash
 func LowerCaseHash(name string) []byte {
-     LowerCaseString := strings.ToLower(name)
-	 return protocol.HashData([]byte(LowerCaseString))
+	LowerCaseString := strings.ToLower(name)
+	return protocol.HashData([]byte(LowerCaseString))
 }
 
 // UpperCaseHash coverts string to upper case and returns the hash
 func UpperCaseHash(name string) []byte {
-    UpperCaseString := strings.ToUpper(name)
+	UpperCaseString := strings.ToUpper(name)
 	return protocol.HashData([]byte(UpperCaseString))
+}
+
+// NormalizeWords Normalizes and sanitizes string passed as the parameter
+func NormalizeWords(text string) (string, error) {
+	// change spaces
+	wordsSplit := WordsSplitString(text)
+
+	// Append string with words with a single space
+	var textWithSpaces string
+	for i := range wordsSplit {
+		if i == 0 {
+			textWithSpaces = wordsSplit[i]
+		} else {
+			textWithSpaces = textWithSpaces + " " + wordsSplit[i]
+		}
+	}
+
+	// Replace _ with a space
+	textWithSpaces = strings.ReplaceAll(textWithSpaces, "_", " ")
+	// Replace - with a space
+	textWithSpaces = strings.ReplaceAll(textWithSpaces, "-", " ")
+
+	// Removing diacritics
+	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
+	result, _, err := transform.String(t, textWithSpaces)
+	if err != nil {
+		return "", err
+	}
+
+	return result, nil
 }
 
 // HashByWordsSplit splits the words in the string
@@ -85,8 +130,13 @@ func HashByWordsSplit(name string) [][]byte {
 		if len(words[i]) <= 3 {
 			continue
 		} else {
-			hashes = append(hashes,  protocol.HashData([]byte(words[i])))
+			hashes = append(hashes, protocol.HashData([]byte(words[i])))
 		}
 	}
 	return hashes
+}
+
+func WordsSplitString(name string) []string {
+	words := strings.Fields(name)
+	return words
 }
--- search/search_test.go
+++ search/search_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 )
 
+// Test written to run the workflow to
+// test generated indexes
 func TestGenerateIndexes(t *testing.T) {
 
 	core.SqliteSearchIndexMigration()
@@ -26,9 +28,26 @@ func TestGenerateIndexes(t *testing.T) {
 
 }
 
+// Test written to run remove Index workflow
 func TestRemoveIndexes(t *testing.T) {
 	err := RemoveIndexes("My Name is Akilan")
 	if err != nil {
 		t.Error(err)
 	}
+}
+
+func TestNormalizeWords(t *testing.T) {
+	words, err := NormalizeWords("français")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(words)
+
+	words, err = NormalizeWords("testé-lol_What to do-idk")
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(words)
+
 }
