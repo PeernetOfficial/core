9775525d35b367b518dda4b3195010c4742823db
Akilan Selvacoumar <31743758+Akilan1999@users.noreply.github.com>
2023-02-12 09:59:31 +0000

    gofmt peernet (#103)

--- webapi/Download_test.go
+++ webapi/Download_test.go
@@ -1,13 +1,13 @@
 package webapi
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 )
 
 // Test function
 func TestDecodeBlake3Hash(t *testing.T) {
-    hash, bool := DecodeBlake3Hash("")
-    fmt.Println(hash)
-    fmt.Println(bool)
+	hash, bool := DecodeBlake3Hash("")
+	fmt.Println(hash)
+	fmt.Println(bool)
 }
